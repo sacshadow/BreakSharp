e3fb7bbd4a4f3dce4ddbb610cf28bde0c5e52721
sacshadow <51149526@qq.com>
2016-08-16 14:07:31 +0800

    d1c1

---
+++ Assets/GameScripts/Action/FAction_Base.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class FAction_Base {
+	
+	public TestManControl control;
+	
+	public FAction_Base(TestManControl control) {
+		this.control = control;
+	}
+	
+	public abstract void BeginAction();
+	
+	public abstract void EndAction();
+	
+	public abstract IEnumerator Process();
+}
---
+++ Assets/GameScripts/TestMan/TestManControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManControl : MonoBehaviour {
+	
+	public TestManAnimation anim;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
