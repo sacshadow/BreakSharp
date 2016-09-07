b4edc85651690b430bb3876ba9abb1d8ee02d294
sacshadow <51149526@qq.com>
2016-09-07 09:38:52 +0800

    Start PhysAnim

---
+++ Assets/GameScripts/PhysAnim/Feet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Feet : MonoBehaviour {
+	
+	
+	public Transform hip;
+	public Transform feet;
+	public LayerMask mask = ~0;
+	
+	public float legLength = 1f;
+	
+	public bool isGround = false;
+	public bool isHited = false;
+	public RaycastHit hit;
+	
+	public Vector3 targetPosition;
+	
+	
+	
+	
+	
+	void LateUpdate() {
+		Vector3 checkPoint = isGround ? groundPoint : feet.position;
+	
+		isHited = Physics.Raycast(hip.position, checkPoint - hip.position, out hit, 1.5f, mask);
+		
+		if(isHited)
+			Debug.DrawLine(hit.position, hit.position + hit.normal);
+		
+		// if(isHited && hit.distance < legLength)
+			// targetPosition = hit.position;
+		
+	}
+	
+}
---
+++ Assets/GameScripts/PhysAnim/PhysAnim.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysAnim : MonoBehaviour {
+	
+	public Rigidbody rBody;
+	
+	public Transform leftFeet, rightFeet;
+	
+	public LayerMask mask = ~0;
+	
+	public float walkSpeed = 1.5f;
+	
+	private Vector3 targetVelocity;
+	
+	// Use this for initialization
+	void Start () {
+		targetVelocity = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float m = Input.GetAxis("Horizontal");
+		
+		
+		targetVelocity.z = Mathf.MoveTowards(targetVelocity.z, m * walkSpeed, 3*Time.deltaTime);
+		// targetVelocity
+		
+		rBody.velocity = targetVelocity;
+		
+	}
+	
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(transform.position, rightFeet.position);
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(transform.position, leftFeet  .position);
+	}
+	
+}
